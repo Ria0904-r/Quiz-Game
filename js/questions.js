2dbdd807b4439771b11672eabbf91d616a5be265
Ria Sehgal <riasehgal48@gmail.com>
2021-06-23 01:22:31 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,124 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Choose the correct HTML tag for the largest heading?",
+    answer: "H1",
+    options: [
+      "H2",
+      "H1",
+      "H3",
+      "H6"
+    ]
+  },
+    {
+    numb: 2,
+    question: "If we want define style for an unique element, then which css selector will we use ?",
+    answer: "Id",
+    options: [
+      "text",
+      "name",
+      "class",
+      "Id"
+    ]
+  },
+    {
+    numb: 3,
+    question: "The CSS property used to make the text bold is -?",
+    answer: "font-weight : bold",
+    options: [
+      "font-weight : bold",
+      "weight: bold",
+      "font: bold",
+      "style: bold"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of the following option leads to the portability and security of Java?",
+    answer: "Bytecode is executed by JVM",
+    options: [
+      "Dynamic binding between objects",
+      "Use of exception handling",
+      "The applet makes the Java code secure and portable",
+      "Bytecode is executed by JVM"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which of the following is not a Java features?",
+    answer: "Use of pointers",
+    options: [
+      "Use of pointers",
+      "Dynamic",
+      "Object-oriented",
+      "Architecture Neutral"
+    ]
+  },
+
+
+    {
+    numb: 6,
+    question: "Which of the following properties will we use to display border around a cell without any content ?",
+    answer: "empty-cell",
+    options: [
+      "empty-cell",
+      "blank-cell",
+      "void-cell",
+      "noncontent-cell"
+    ]
+  },
+  {
+  numb: 7,
+  question: "Which of the following property is used to indicate whether the backside of an element is visible if the element is rotated to display the back ?",
+  answer: " backface-visibility",
+  options: [
+    "background-clip",
+    "behavior",
+    " backface-visibility",
+    " background-origin"
+
+  ]
+},
+
+{
+numb: 8,
+question: "Which attribute is used to provide an advisory text about an element or its contents ?",
+answer: "title",
+options: [
+  "title",
+  "head",
+  "tooltip",
+  "dir"
+]
+},
+
+
+{
+numb: 9,
+question: "Which image file format is not supported in Firefox?",
+answer: "MNG",
+options: [
+  "SVG",
+  "PNG",
+  "MNG",
+  "APNG"
+]
+},
+
+{
+numb: 10,
+question: "Which of the following can’t be the value of list-style-type?",
+answer: "ellipse",
+options: [
+  "disc",
+  "ellipse",
+  "circle",
+  "square"
+]
+},
+
+
+
+
+];
